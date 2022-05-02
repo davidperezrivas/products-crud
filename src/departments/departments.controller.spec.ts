e159dffcce65800befa75a6ccf1b0d86e79977e3
David Perez <davip.rivas@gmail.com>
2022-05-02 10:36:12 -0400

    solved linter/prittier errors

--- src/departments/departments.controller.spec.ts
+++ src/departments/departments.controller.spec.ts
@@ -3,18 +3,18 @@ import { DepartmentsController } from './departments.controller';
 import { DepartmentsService } from './departments.service';
 
 describe('DepartmentsController', () => {
-  let controller: DepartmentsController;
+    let controller: DepartmentsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [DepartmentsController],
-      providers: [DepartmentsService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [DepartmentsController],
+            providers: [DepartmentsService],
+        }).compile();
 
-    controller = module.get<DepartmentsController>(DepartmentsController);
-  });
+        controller = module.get<DepartmentsController>(DepartmentsController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
--- src/departments/departments.service.ts
+++ src/departments/departments.service.ts
@@ -4,23 +4,23 @@ import { UpdateDepartmentDto } from './dto/update-department.dto';
 
 @Injectable()
 export class DepartmentsService {
-  create(createDepartmentDto: CreateDepartmentDto) {
-    return 'This action adds a new department';
-  }
+    create(createDepartmentDto: CreateDepartmentDto) {
+        return 'This action adds a new department';
+    }
 
-  findAll() {
-    return `This action returns all departments`;
-  }
+    findAll() {
+        return 'This action returns all departments';
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} department`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} department`;
+    }
 
-  update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
-    return `This action updates a #${id} department`;
-  }
+    update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
+        return `This action updates a #${id} department`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} department`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} department`;
+    }
 }
