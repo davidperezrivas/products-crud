86302c80420fa1360ff104de262e7ad574402ae3
David Perez <davip.rivas@gmail.com>
2022-05-01 22:37:54 -0400

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,13 @@ import { DepartmentsModule } from './departments/departments.module';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-const URL_DB = process.env.NODE_ENV == 'test' ? process.env.DB_URL_TEST : process.env.DB_URL;
+const URL_DB = process.env.NODE_ENV == 'test' ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL;
 console.log(URL_DB);
-console.log(process.env.DB_URL_TEST);
-console.log(process.env.DB_URL);
+console.log(process.env.DATABASE_URL_TEST);
+console.log(process.env.DATABASE_URL);
 
 @Module({
-    imports: [
-        MongooseModule.forRoot(
-            'mongodb+srv://david:Pd4ufbIXQrQhPW9E@cluster0.qfdlg.mongodb.net/farmacy_test?retryWrites=true&w=majority',
-        ),
-        DepartmentsModule,
-        ProductsModule,
-    ],
+    imports: [MongooseModule.forRoot(URL_DB), DepartmentsModule, ProductsModule],
     controllers: [AppController],
     providers: [],
 })
