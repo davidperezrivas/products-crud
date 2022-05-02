215014ad073efba41847214579a4b93635c1af23
David Perez <davip.rivas@gmail.com>
2022-05-01 22:11:52 -0400

    mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DepartmentsModule } from './departments/departments.module';
 import { ProductsModule } from './products/products.module';
-//import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
-//const URL_DB = process.env.NODE_ENV == 'test' ? process.env.DB_URL_TEST : process.env.DB_URL;
+const URL_DB = process.env.NODE_ENV == 'test' ? process.env.DB_URL_TEST : process.env.DB_URL;
 
 @Module({
-    imports: [DepartmentsModule, ProductsModule],
+    imports: [MongooseModule.forRoot(URL_DB), DepartmentsModule, ProductsModule],
     controllers: [AppController],
     providers: [],
 })
