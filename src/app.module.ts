201c9d333f0f9058619cffb852c4600548715df0
David Perez <davip.rivas@gmail.com>
2022-05-01 21:52:58 -0400

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DepartmentsModule } from './departments/departments.module';
 import { ProductsModule } from './products/products.module';
-import { MongooseModule } from '@nestjs/mongoose';
+//import { MongooseModule } from '@nestjs/mongoose';
 
-const URL_DB = process.env.NODE_ENV == 'test' ? process.env.DB_URL_TEST : process.env.DB_URL;
+//const URL_DB = process.env.NODE_ENV == 'test' ? process.env.DB_URL_TEST : process.env.DB_URL;
 
 @Module({
     imports: [DepartmentsModule, ProductsModule],
