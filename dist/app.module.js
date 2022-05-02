ef4365cf1594af8d1f01ac4f0d1d54c5c1ef73d9
David Perez <davip.rivas@gmail.com>
2022-05-01 21:59:14 -0400

    update

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,15 +8,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
+const departments_module_1 = require("./departments/departments.module");
+const products_module_1 = require("./products/products.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        imports: [departments_module_1.DepartmentsModule, products_module_1.ProductsModule],
+        controllers: [],
+        providers: [],
     })
 ], AppModule);
 exports.AppModule = AppModule;
